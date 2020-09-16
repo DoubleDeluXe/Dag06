62907ac546d1240af616ef25a73669e931434f38
Dave Levi <Dave.v.Toor@googlemail.com>
2020-09-16 10:47:34 +0200

    omringde3hoek

--- src/test/java/org/example/DaveLevi/Dag06/Dag06.java
+++ src/test/java/org/example/DaveLevi/Dag06/Dag06.java
@@ -7,31 +7,39 @@ public class Dag06 {
     @Test
     void rechterDriehoek() {
         int size = 5;
-
+        printAt();
+        System.out.println();
         for (int i = 0; i < size; i++) {
             rechterDriehoekOpbouwen(i);
             System.out.println();
         }
         for (int i = 0; i < size; i++) {
-            rechterDriehoekAfbouwen(i, size);
+            rechterDriehoekAfbouwen(i, size-1);
             System.out.println();
         }
+        printAt();
     }
 
     void rechterDriehoekOpbouwen(int i) {
+        printAt();
         for (int j = 0; j <= i; j++) {
             printStar();
         }
+        printAt();
     }
     void rechterDriehoekAfbouwen(int i, int size) {
-        size = size - i;
-        for (int j = size; j <= i; i++){
+        printAt();
+        for (int j = size; i < j; j = j -1){
             printStar();
         }
+        printAt();
     }
 
     void printStar() {
         System.out.print("* ");
+    }
+    void printAt() {
+        System.out.print("@");
     }
 
 }
