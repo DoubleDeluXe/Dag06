2307d990483dc82eddf51f301cdf841571422eb8
Dave Levi <Dave.v.Toor@googlemail.com>
2020-09-17 10:13:47 +0200

    x

---
+++ src/test/java/org/example/DaveLevi/Dag06/DriehoekBovenEnBeneden.java
@@ -0,0 +1,55 @@
+package org.example.DaveLevi.Dag06;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class DriehoekBovenEnBeneden {
+
+    @Test
+    void printDriehoekMetPuntNaarBoven() {
+        Scanner invul = new Scanner(System.in);
+
+        System.out.println("Voer het aantal rijen in dat je wilt hebben ");
+
+        int aantalRijen = invul.nextInt();
+
+        for (int index = 1; index <= aantalRijen; index++) {
+            for (int ruimteIndex = index; ruimteIndex < aantalRijen; ruimteIndex++) {
+                System.out.print(" ");
+            }
+            for (int printXIndex = 1; printXIndex <= (2 * index - 1); printXIndex++) {
+                if (printXIndex == 1 || index == aantalRijen || printXIndex == (2 * index - 1)) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        invul.close();
+    }
+
+    @Test
+    void printDriehoekMetPuntNaarBenenden() {
+        Scanner invul = new Scanner(System.in);
+        System.out.println("Voer het aantal rijen in dat je wilt hebben ");
+
+        int aantalRijen = invul.nextInt();
+        for (int index = aantalRijen; index >= 1; index--) {
+            for (int ruimteIndex = index; ruimteIndex < aantalRijen; ruimteIndex++) {
+                System.out.print(" ");
+            }
+            for (int printXIndex = 1; printXIndex <= (2 * index - 1); printXIndex++) {
+                if (printXIndex == 1 || index == aantalRijen || printXIndex == (2 * index - 1)) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        invul.close();
+    }
+
+}
